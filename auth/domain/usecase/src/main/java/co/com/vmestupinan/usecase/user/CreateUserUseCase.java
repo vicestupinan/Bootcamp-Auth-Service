1090dfb7dcce7056ec18c0ce91a5b62a667fce91
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-08-28 09:44:24 -0500

    refactor(usecase): rename UserUseCase to CreateUserUseCase

--- auth/domain/usecase/src/main/java/co/com/vmestupinan/usecase/user/CreateUserUseCase.java
+++ auth/domain/usecase/src/main/java/co/com/vmestupinan/usecase/user/CreateUserUseCase.java
@@ -4,25 +4,25 @@ import java.math.BigDecimal;
 
 import co.com.vmestupinan.model.user.User;
 import co.com.vmestupinan.model.user.gateways.UserRepository;
-import co.com.vmestupinan.usecase.user.exception.BusinessException;
+import co.com.vmestupinan.usecase.user.exception.EmailAlreadyExistsException;
+import co.com.vmestupinan.usecase.user.exception.InvalidSalaryException;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
-public class UserUseCase {
+public class CreateUserUseCase {
 
     private final UserRepository userRepository;
 
-    public Mono<User> create(User user) {
-        return userRepository.findByEmail(user.getEmail())
-                .hasElement()
+    public Mono<User> execute(User user) {
+        return userRepository.existsByEmail(user.getEmail())
                 .flatMap(exists -> {
                     if (exists) {
-                        return Mono.error(new BusinessException("El correo ya está registrado"));
+                        return Mono.error(new EmailAlreadyExistsException("El correo ya está registrado"));
                     }
                     if (user.getBaseSalary().compareTo(BigDecimal.ZERO) < 0
                             || user.getBaseSalary().compareTo(new BigDecimal("15000000")) > 0) {
-                        return Mono.error(new BusinessException("El salario base debe estar entre 0 y 15,000,000"));
+                        return Mono.error(new InvalidSalaryException("El salario base debe estar entre 0 y 15,000,000"));
                     }
                     return userRepository.save(user);
                 });
